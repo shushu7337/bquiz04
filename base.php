215b3f42c5ec8b92bb55019d049ebab40fe0b91e
shuchang <dreamsky1208@gmail.com>
2020-07-29 14:16:15 +0800

    初始化、切版、建立資料表

---
+++ base.php
@@ -0,0 +1,96 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB{
+    private $dsn="mysql:host=localhost;charset=utf8;dbname=db08";
+    private $root="root";
+    private $password="";
+    private $table;
+    private $pdo;
+
+    public function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,$this->root,$this->password);
+    }
+
+    public function all(...$arg){
+        $sql="SELECT * FROM $this->table ";
+        if(!empty($arg[0]) && is_array($arg[0])){
+            foreach($arg[0] as $key => $value){
+                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            }
+            $sql=$sql." where ".implode(" && ",$tmp);
+        }
+        if(!empty($arg[1])){
+            $sql=$sql.$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchAll();
+    }
+
+    public function count(...$arg){
+        $sql="SELECT COUNT(*) FROM $this->table ";
+        if(!empty($arg[0]) && is_array($arg[0])){
+            foreach($arg[0] as $key => $value){
+                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            }
+            $sql=$sql." where ".implode(" && ",$tmp);
+        }
+        if(!empty($arg[1])){
+            $sql=$sql.$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    public function find($arg){
+        $sql="SELECT * FROM $this->table ";
+        if(is_array($arg)){
+            foreach($arg as $key => $value){
+                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            }
+            $sql=$sql." where ".implode(" && ",$tmp);
+        }else{
+            $sql=$sql." where `id`='$arg'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function del($arg){
+        $sql="DELETE FROM $this->table ";
+        if(is_array($arg)){
+            foreach($arg as $key => $value){
+                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            }
+            $sql=$sql." where ".implode(" && ",$tmp);
+        }else{
+            $sql=$sql." where `id`='$arg'";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    public function save($arg){
+        if(!epmty($arg['id'])){
+            foreach($arg as $key => $value){
+                if($key!='id'){
+                    $tmp[]=sprintf("`%s`='%s'",$key,$value);
+                }
+            }
+            $sql="UPDATE　$this->table SET ".impldo(",",$tmp)." where `id`='".$arg['id']."'";
+        }else{
+            $sql="INSERT INTO $this->table (`".implode("`,`",array_keys($arg))."`) VALUES('".implode("','",$arg)."')";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    public function q($arg){
+        return $this->pdo->query($sql)->fetchAll();
+    }
+}
+
+function to($url){
+    header("location:$url");
+}
+
+
+
+?>
