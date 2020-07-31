a0e46fdca88efcb5078899a01483c3700d444600
shuchang <dreamsky1208@gmail.com>
2020-07-31 15:38:23 +0800

    完成詳細商品資料功能

---
+++ front/detail.php
@@ -0,0 +1,37 @@
+<?php
+$goods=$Goods->find($_GET['id']);
+?>
+<h2 class="ct"><?=$goods['name'];?></h2>
+<div style='display:flex;width:80%;margin:10px auto' class="pp">
+    <div style='width:40%;display:flex;justify-content:center;align-items:center;text-align:center;border-right:3px solid white;padding:10px;'>
+        <a href="?do=detail&id=<?=$goods['id'];?>"><img src="img/<?=$goods['img'];?>" style="width:80%"></a>
+    </div>
+    <div style='width:60%;'>
+        <div style="border-bottom:3px solid white;padding:10px">分類:<?=$Type->find($goods['big'])['name'];?>><?=$Type->find($goods['mid'])['name'];?></div>
+        <div style="border-bottom:3px solid white;padding:10px">編號:<?=$goods['no'];?></div>
+        <div style="border-bottom:3px solid white;padding:10px">價格:<?=$goods['price'];?></div>
+        <div style="border-bottom:3px solid white;padding:10px">詳細說明:<?=nl2br($goods['intro']);?></div>
+        <div>庫存量:<?=$goods['stock'];?></div>
+    </div>
+</div>
+<div class="tt ct" style="width:80%;padding:10px 0;margin:auto;">
+    <input type="number" name="qt" id="qt" style="width:30px" value="1">
+    <input type="hidden" name="id" id="id" value="<?=$goods['id'];?>">
+    <a href="javascript:buy()"><img src="icon/0402.jpg" alt=""></a>
+</div>
+<div class="ct">
+    <button onclick="location.href='index.php'">返回</button>
+</div>
+
+<script>
+    function buy(){
+        let 
+            id=$("#id").val(),
+            qt=$("#qt").val();
+        location.href=`?do=login&id=${id}&qt=${qt}`;
+            
+    }
+
+
+
+</script>
--- front/main.php
+++ front/main.php
@@ -1,12 +1,27 @@
+<?php
+    $type=(!empty($_GET['type']))?$_GET['type']:"0";    //得到首頁傳來的type值  (這裡傳過來的type值即為type資料表中的id值)
+    if(empty($type)){       
+        $goods=$Goods->all(['sh'=>1]);  //如果值為空的話，就撈出所有商品
+        $nav='全部商品';
+    }else{
+        $tt=$Type->find($type); //從type資料表中 找(傳過來的id值)的資料
+        if($tt['parent']==0){   //此處的tt 為 type 資料表中的 parent值
+            $goods=$Goods->all(['sh'=>1,'big'=>$type]); //撈出大分類商品    (如果parent為0的話表示他是大分類 沒有中分類所以只撈出大分類)
+            $nav=$tt['name'];
+        }else{
+            $goods=$Goods->all(['sh'=>1,'mid'=>$type]); //撈出中分類商品    (如果parent 不為0的話 就是中分類項目)
+            $nav=$Type->find($tt['parent'])['name']. " > " . $tt['name'];
+        }
+    }
+?>
+
 <h1>全部商品</h1>
 <?php
-    $goods=$Goods->all(['sh'=>1]);
     foreach($goods as $g){
-        ?>
-
+?>
 <div style='display:flex;width:80%;margin:10px auto' class="pp">
-        <div style='width:40%;display:flex;justify-content:center;align-items:center;'>
-            <img src="img/<?=$g['img'];?>" style="width:80%">
+        <div style='width:40%;display:flex;justify-content:center;align-items:center;text-align:center'>
+            <a href="?do=detail&id=<?=$g['id'];?>"><img src="img/<?=$g['img'];?>" style="width:80%"></a>
         </div>
         <div style='width:60%;'>
             <div class="tt"><?=$g['name'];?></div>
@@ -16,7 +31,6 @@
             <div>規格:<?=$g['spec'];?></div>
             <div>簡介:<?=mb_substr($g['intro'],0,25,'utf8');?>...</div>
         </div>
-        
 </div>
 
 <?php
