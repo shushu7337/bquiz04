14b668267d03edfea9fce94c5bdfcc5b60c0c53f
shuchang <dreamsky1208@gmail.com>
2020-07-31 14:29:28 +0800

    顯示全部商品

--- front/main.php
+++ front/main.php
@@ -1,1 +1,24 @@
-主內容區
+<h1>全部商品</h1>
+<?php
+    $goods=$Goods->all(['sh'=>1]);
+    foreach($goods as $g){
+        ?>
+
+<div style='display:flex;width:80%;margin:10px auto' class="pp">
+        <div style='width:40%;display:flex;justify-content:center;align-items:center;'>
+            <img src="img/<?=$g['img'];?>" style="width:80%">
+        </div>
+        <div style='width:60%;'>
+            <div class="tt"><?=$g['name'];?></div>
+            <div>價格:<?=$g['price'];?>
+                <a href="?do=login"><img src="icon/0402.jpg" style="float:right;"></a>
+            </div>
+            <div>規格:<?=$g['spec'];?></div>
+            <div>簡介:<?=mb_substr($g['intro'],0,25,'utf8');?>...</div>
+        </div>
+        
+</div>
+
+<?php
+    }
+?>
